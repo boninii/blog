78e74176fe24f9c87ebc4a44ef8835ed2fc5ef11
bonini <wakandadoceu@gmail.com>
2022-08-03 11:42:21 -0300

    blog

--- core/mysql.php
+++ core/mysql.php
@@ -48,6 +48,98 @@
             $tipo[] = gettype($dado) [0];
             $expressao[count($expressao) -1] = '?';
             $coringa_criterio[] = $expressao;
+
+            $nome_campo = (count($expressao) < 4) ? $expressao[0] : $expressao[1];
+
+            if(isset($nome_campo)) {
+                $nome_campo= $nome_campo . '_' . rand();
+            }
+
+            $campos_criterio[] = $nome_campo;
+
+            $$nome_campo = $dado;
         }
+
+        $instrucao = update($entidade, $coringa_dados, $coringa_criterio);
+
+        $conexao = conecta();
+
+        $stmt = mysqli_prepare($conexao, $instrucao);
+
+        if(isset($tipo)) {
+            $comando = 'mysqli_stmt_bind_param($stmt,';
+            $comando .= "'" . implode('', $tipo). "'";
+            $comando .= ', $' . implode(', $', array_keys($dados));
+            $comando .= ', $' . implode(', $', $campos_criterio);
+            $comando .= ');';
+
+            eval($comando);
+        }
+
+        mysqli_stmt_execute($stmt);
+
+        $retorno = (boolean) mysqli_stmt_affected_rows($stmt);
+
+        $_SESSION['errors'] = mysqli_stmt_error_list($stmt);
+
+        mysqli_stmt_close($stmt);
+
+        desconecta($conexao);
+
+        return $retorno;
+    }
+
+    function deleta(string $entidade, array $criterio = []) : bool
+    {
+        $retorno = false;
+
+        $coringa_criterio = [];
+
+        foreach ($criterio as $expressao) {
+            $dado = $expressao[count($expressao) -1];
+
+            $tipo[] = gettype($dado) [0];
+            $expressao[count($expressao) -1] = '?';
+            $coringa_criterio[] = $expressao;
+
+            $nome_campo = (count($expressao) < 4 ) ? $expressao[0] : $expressao[1];
+
+            $campos_criterio[] = $nome_campo;
+
+            $$nome_campo = $dado;
+        }
+
+        $instrucao = delete($entidade, $coringa_criterio);
+
+        $conexao =  conecta();
+
+        $stmt = mysqli_prepare($conexao, $instrucao);
+
+        if(isset($tipo)) {
+            $comando = 'mysqli_stmt_bind_param($stmt,';
+            $comando .= "'" . implode('', $tipo). "'";
+            $comando .= ', $' . implode(', $', $campos_criterio);
+            $comando .= ');';
+
+            eval($comando);
+        }
+
+        mysqli_stmt_execute($stmt);
+
+        $retorno = (boolean) mysqli_stmt_affected_rows($stmt);
+
+        $_SESSION['errors'] = mysqli_stmt_error_list($stmt);
+
+        mysqli_stmt_close($stmt);
+
+        desconecta($conexao);
+
+        return $retorno;
+    }
+
+    function buscar(string $entidade, array $campos =['*'], array $criterio = [],
+    string $ordem = null) : array
+    {
+        
     }
 ?>
