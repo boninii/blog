86fc52eea6b79ed06b38e46f166c29d200d0d560
bonini <wakandadoceu@gmail.com>
2022-09-14 11:10:19 -0300

    blog

--- post_formulario.php
+++ post_formulario.php
@@ -55,10 +55,58 @@
                                 value="<?php echo $entidade['id'] ?? '' ?>">
                         <div class="form-group">
                             <label for="titulo">Título</label>
+                            <input class="form-control" type="text"
+                                    require="required" id="titulo" name="titulo"
+                                    value="<?php echo $entidade['titulo'] ?? ''?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="texto">Texto</label>
+                            <textarea class="form-control" type="text"
+                                    require="required" id="texto" name="texto" rows="5">
+                                <?php echo $entidade['texto'] ?? ''?>
+                            </textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="texto">Postar em</label>
+                            <?php
+                                $data = (!empty($entidade['data_postagem']))?
+                                    explode(' ', $entidade['data_postagem'])[0] : '';
+                                $hora = (!empty($entidade['data_postagem']))?
+                                    explode(' ', $entidade['data_postagem'])[1] : '';
+
+                            ?>
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <input class="form-control" type="date"
+                                        require="required"
+                                        id="data_postagem"
+                                        name="data_postagem"
+                                        value="<?php echo $data ?>">
+                                </div>
+                                <div class="col-md-3">
+                                    <input class="form-control" type="time"
+                                        require="required"
+                                        id="hora_postagem"
+                                        name="hora_postagem"
+                                        value="<?php echo $hora ?>">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="text-right">
+                            <button class="btn btn-success"
+                                    type="submit">Salvar</button>
                         </div>
                     </form>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <?php
+                        include 'includes/rodape.php';
+                    ?>
+                </div>
+            </div>
         </div>
+        <script src="lib/bootstrap-4.2.1-dist/js/bootstrap.min.js"></script>
     </body>
 </html>
