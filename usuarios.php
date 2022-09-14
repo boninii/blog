6252efaa4290d2bdc73997aaa45eeca0ee9cafba
bonini <wakandadoceu@gmail.com>
2022-09-14 11:36:10 -0300

    blog

--- usuarios.php
+++ usuarios.php
@@ -43,10 +43,62 @@
                             $criterio [] = ['nome', 'like', "%{busca}%"];
                         }
 
-                        
+                        $result = buscar(
+                            'usuario',
+                            [
+                                'id',
+                                'nome',
+                                'email',
+                                'data_criacao',
+                                'ativo',
+                                'adm'
+                            ],
+                            $criterio,
+                            'data_criacao DESC, nome ASC'
+                        );
+
+                    ?>
+                    <table class="table table-bordered table-hover table-striped 
+                                    table-responsive{-sm|-md|-lg|-xl}">
+                        <thead>
+                            <tr>
+                                <td>Nome</td>
+                                <td>E-mail</td>
+                                <td>Data criacao</td>
+                                <td>Ativo</td>
+                                <td>Administrador</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                foreach($result as $entidade):
+                                    $data = date_create($entidade['data_criacao']);
+                                    $data = date_format($data, 'd/m/Y H:i:s');
+                            ?>
+                            <tr>
+                                <td><?php echo $entidade['nome'] ?></td>
+                                <td><?php echo $entidade['email'] ?></td>
+                                <td><?php echo $data ?></td>
+                                <td><a href='core/usuario_repositorio.php?acao=status&id=<?php echo $entidade['id']?>
+                                &valor=<?php echo !$entidade['ativo']?>'><?php ($entidade['ativo']==1) ? 
+                                'Desativar' : 'Ativar'; ?></a></td>
+                                <td><a href='core/usuario_repositorio.php?acao=adm&id=<?php echo $entidade['id']?>
+                                &valor=<?php echo !$entidade['adm']?>'><?php ($entidade['adm']==1) ? 'Rebaixar'
+                                : 'Promover'; ?></a></td>
+                            </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table> 
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <?php
+                        include 'includes/rodape.php';
                     ?>
                 </div>
             </div>
         </div>
+        <script src="lib/bootstrap-4.2.1-dist/js/bootstrap.min.js"></script>
     </body>
 </html>
